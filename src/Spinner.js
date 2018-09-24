510fd35a7b0f2eb6dbdedeeba25dc017e8e14dde
Carlos Filoteo <c.filoteo@live.com>
2018-09-23 21:47:57 -0600

    Add Spinner component

---
+++ src/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Spinner.css'
+
+const Spinner = () => 
+  <span className="spinner" aria-busy="true" role="alert">
+    <span className="hideVisually">Loading...</span>
+  </span>
+
+export default Spinner
