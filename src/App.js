c422013cdc05e474f72e80d1bbca38e59f9f3a8c
Carlos Filoteo <c.filoteo@live.com>
2018-09-23 21:51:27 -0600

    Load content in App and render reviews

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './App.css';
+import Spinner from './Spinner'
+import {getReviews} from './resource'
 
 class App extends Component {
+  state = {
+    reviews: null,
+  }
+  componentDidMount() {
+    getReviews.then(({data: reviews}) => {
+      this.setState({reviews})
+    })
+  }
   render() {
+    const {reviews} = this.state
     return (
       <div className="App">
         <nav className="nav">
           <h1 className="title">Shakespeare Reviews</h1>
         </nav>
+        <main>
+          {reviews === null 
+            ? <Spinner/>
+            : reviews.map(review => <div key={review.id}></div>)}
+        </main>
       </div>
     );
   }
