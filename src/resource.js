cdc944ed66b3e799fdfaee301073a92ca33a96d1
Carlos Filoteo <c.filoteo@live.com>
2018-09-24 23:44:06 -0600

    Fix formatting; add getReviewById resource

--- src/resource.js
+++ src/resource.js
@@ -1,12 +1,18 @@
-const API_KEY = process.env.REACT_APP_API_AUTH_TOKEN
+const AUTH_TOKEN = process.env.REACT_APP_API_AUTH_TOKEN
 
 const options = {
   headers: new Headers({
-    'Authorization': API_KEY,
+    Authorization: AUTH_TOKEN,
     'Content-Type': 'application/json',
   }),
 }
 
-export const getReviews = 
-  fetch('http://shakespeare.podium.co/api/reviews', options)
-    .then(res => res.json())
+export const getReviews = fetch(
+  'http://shakespeare.podium.co/api/reviews',
+  options
+).then(res => res.json())
+
+export const getReviewById = id =>
+  fetch(`http://shakespeare.podium.co/api/reviews/${id}`, options).then(res =>
+    res.json()
+  )
