907af412e020fe94c02e5502969dbdaa96e101ce
Carlos Filoteo <c.filoteo@live.com>
2018-09-24 23:46:12 -0600

    Formatting; add loadSingleReview to get a single review description

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react'
-import './App.css';
+import './App.css'
 import Spinner from './Spinner'
-import {getReviews} from './resource'
+import Review from './Review'
+import { getReviews, getReviewById } from './resource'
 
 class App extends Component {
   state = {
     reviews: null,
   }
   componentDidMount() {
-    getReviews.then(({data: reviews}) => {
-      this.setState({reviews})
+    getReviews.then(({ data: reviews }) => {
+      this.setState({ reviews })
+    })
+  }
+  loadSingleReview = reviewIndex => {
+    const singleReview = this.state.reviews[reviewIndex]
+    getReviewById(singleReview.id).then(({ data: review }) => {
+      const reviews = [...this.state.reviews]
+      reviews[reviewIndex] = { ...singleReview, review }
+      this.setState({ reviews })
     })
   }
   render() {
-    const {reviews} = this.state
+    const { reviews } = this.state
     return (
       <div className="App">
         <nav className="nav">
           <h1 className="title">Shakespeare Reviews</h1>
         </nav>
         <main>
-          {reviews === null 
-            ? <Spinner/>
-            : reviews.map(review => <div key={review.id}></div>)}
+          {reviews === null ? (
+            <Spinner />
+          ) : (
+            reviews.map(review => <Review key={review.id} {...review} />)
+          )}
         </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
