fb33b38fde693c276fb80740b34c8f7200424343
Carlos Filoteo <c.filoteo@live.com>
2018-09-27 01:00:24 -0600

    Refactor mocks to allow for better comparisons

--- src/App.test.js
+++ src/App.test.js
@@ -3,39 +3,43 @@ import { shallow } from 'enzyme'
 import App from './App'
 import { getReviews, getReviewById } from './resource'
 
+const getReviewsData = {
+  data: [
+    {
+      rating: 0.8,
+      publish_date: '2016-09-05T23:25:47.642350Z',
+      id: '9783221620868',
+      author: 'Kaley Schiller',
+    },
+    {
+      rating: 3.2,
+      publish_date: '2016-09-04T23:25:47.642388Z',
+      id: '9793364045824',
+      author: 'Fay Lemke',
+    },
+  ],
+}
+
+const getReviewByIdData = {
+  data: {
+    rating: 3.2,
+    publish_date: '2016-09-04T23:25:47.642388Z',
+    id: '9793364045824',
+    body: 'Can one desire too much of a good thing?.',
+    author: 'Fay Lemke',
+  },
+}
+
 jest.mock('./resource', () => ({
-  getReviews: jest.fn(() =>
-    Promise.resolve({
-      data: [
-        {
-          rating: 0.8,
-          publish_date: '2016-09-05T23:25:47.642350Z',
-          id: '9783221620868',
-          author: 'Kaley Schiller',
-        },
-        {
-          rating: 3.2,
-          publish_date: '2016-09-04T23:25:47.642388Z',
-          id: '9793364045824',
-          author: 'Fay Lemke',
-        },
-      ],
-    })
-  ),
-  getReviewById: jest.fn(() =>
-    Promise.resolve({
-      data: {
-        rating: 3.2,
-        publish_date: '2016-09-04T23:25:47.642388Z',
-        id: '9793364045824',
-        body: 'Can one desire too much of a good thing?.',
-        author: 'Fay Lemke',
-      },
-    })
-  ),
+  getReviews: jest.fn(),
+  getReviewById: jest.fn(),
 }))
 
 describe('<App/>', () => {
+  beforeAll(() => {
+    getReviews.mockImplementation(() => Promise.resolve(getReviewsData))
+    getReviewById.mockImplementation(() => Promise.resolve(getReviewByIdData))
+  })
   it('renders without crashing', () => {
     const wrapper = shallow(<App />)
     expect(wrapper).toBeTruthy()
@@ -48,12 +52,19 @@ describe('<App/>', () => {
   })
   describe('with data', () => {
     let wrapper
-    beforeEach(async () => {
+    beforeEach(() => {
       wrapper = shallow(<App />)
-      await Promise.resolve()
     })
     it('displays a Review for each item in data response', () => {
-      expect(wrapper.find('Review').length).toEqual(2)
+      expect(wrapper.find('Review').length).toEqual(getReviewsData.data.length)
+    })
+    it('calls loads single review body', async () => {
+      const reviewIndex = 1
+      wrapper.instance().loadSingleReview(reviewIndex)
+      await Promise.resolve()
+      expect(wrapper.state('reviews')[reviewIndex].body).toEqual(
+        getReviewByIdData.data.body
+      )
     })
   })
 })
