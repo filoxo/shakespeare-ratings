20b6b2cf8a1f83ba47d56cbc939f74da2369ea9f
Carlos Filoteo <c.filoteo@live.com>
2018-09-27 00:39:43 -0600

    Test with and without mock fetch reqeusts

--- src/App.test.js
+++ src/App.test.js
@@ -1,36 +1,59 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import App from './App'
+import { getReviews, getReviewById } from './resource'
 
-const mockData = {
-  data: [
-    {
-      rating: 0.8,
-      publish_date: '2016-09-05T23:25:47.642350Z',
-      id: '9783221620868',
-      author: 'Kaley Schiller',
-    },
-    {
-      rating: 3.2,
-      publish_date: '2016-09-04T23:25:47.642388Z',
-      id: '9793364045824',
-      author: 'Fay Lemke',
-    },
-  ],
-}
+jest.mock('./resource', () => ({
+  getReviews: jest.fn(() =>
+    Promise.resolve({
+      data: [
+        {
+          rating: 0.8,
+          publish_date: '2016-09-05T23:25:47.642350Z',
+          id: '9783221620868',
+          author: 'Kaley Schiller',
+        },
+        {
+          rating: 3.2,
+          publish_date: '2016-09-04T23:25:47.642388Z',
+          id: '9793364045824',
+          author: 'Fay Lemke',
+        },
+      ],
+    })
+  ),
+  getReviewById: jest.fn(() =>
+    Promise.resolve({
+      data: {
+        rating: 3.2,
+        publish_date: '2016-09-04T23:25:47.642388Z',
+        id: '9793364045824',
+        body: 'Can one desire too much of a good thing?.',
+        author: 'Fay Lemke',
+      },
+    })
+  ),
+}))
 
 describe('<App/>', () => {
-  beforeEach(() => {
-    mock.mockClear()
-  })
   it('renders without crashing', () => {
-    fetch.mockReturnValueOnce(
-      Promise.resolve({
-        ok: true,
-        json: () => mockdata,
-      })
-    )
     const wrapper = shallow(<App />)
     expect(wrapper).toBeTruthy()
+  })
+  describe('without data', () => {
+    it('should display a Spinner if data has not loaded', () => {
+      const wrapper = shallow(<App />)
+      expect(wrapper.exists('Spinner')).toBeTruthy()
+    })
+  })
+  describe('with data', () => {
+    let wrapper
+    beforeEach(async () => {
+      wrapper = shallow(<App />)
+      await Promise.resolve()
+    })
+    it('displays a Review for each item in data response', () => {
+      expect(wrapper.find('Review').length).toEqual(2)
+    })
   })
 })
