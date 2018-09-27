a4d7f231dbfe8c8532630262428547e1eb9c7c68
Carlos Filoteo <c.filoteo@live.com>
2018-09-26 23:27:45 -0600

    Add smoke test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+const mockData = {
+  data: [
+    {
+      rating: 0.8,
+      publish_date: '2016-09-05T23:25:47.642350Z',
+      id: '9783221620868',
+      author: 'Kaley Schiller',
+    },
+    {
+      rating: 3.2,
+      publish_date: '2016-09-04T23:25:47.642388Z',
+      id: '9793364045824',
+      author: 'Fay Lemke',
+    },
+  ],
+}
+
+describe('<App/>', () => {
+  beforeEach(() => {
+    mock.mockClear()
+  })
+  it('renders without crashing', () => {
+    fetch.mockReturnValueOnce(
+      Promise.resolve({
+        ok: true,
+        json: () => mockdata,
+      })
+    )
+    const wrapper = shallow(<App />)
+    expect(wrapper).toBeTruthy()
+  })
 })
