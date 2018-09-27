78d294e3cbb00344b91147c29e9fc1070db78454
Carlos Filoteo <c.filoteo@live.com>
2018-09-26 22:44:57 -0600

    Provide loadReviewBody to individual Reviews to request load from parent App

--- src/App.js
+++ src/App.js
@@ -9,15 +9,20 @@ class App extends Component {
     reviews: null,
   }
   componentDidMount() {
-    getReviews.then(({ data: reviews }) => {
-      this.setState({ reviews })
-    })
+    getReviews
+      .then(({ data: reviews }) => {
+        this.setState({ reviews })
+      })
+      .catch(err => {
+        this.setState({ err })
+      })
   }
   loadSingleReview = reviewIndex => {
     const singleReview = this.state.reviews[reviewIndex]
     getReviewById(singleReview.id).then(({ data: review }) => {
       const reviews = [...this.state.reviews]
-      reviews[reviewIndex] = { ...singleReview, review }
+      const { body } = review
+      reviews[reviewIndex] = { ...singleReview, body }
       this.setState({ reviews })
     })
   }
@@ -32,7 +37,13 @@ class App extends Component {
           {reviews === null ? (
             <Spinner />
           ) : (
-            reviews.map(review => <Review key={review.id} {...review} />)
+            reviews.map((review, i) => (
+              <Review
+                key={review.id}
+                loadReviewBody={() => this.loadSingleReview(i)}
+                {...review}
+              />
+            ))
           )}
         </main>
       </div>
